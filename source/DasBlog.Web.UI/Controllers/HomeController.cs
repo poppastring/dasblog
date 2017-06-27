421ba0c850cd8d0ec40106367578453c56de9049
Mark Downie <mdownie@poppastring.com>
2017-06-26 22:40:03 -0400

    Framing out MVC based UI

---
+++ source/DasBlog.Web.UI/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DasBlog.Web.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Main Page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Page(int page)
+        {
+            // 0 = main page
+
+            return View();
+        }
+
+        // Individual Page + Comment View
+        public IActionResult BlogPost(string article)
+        {
+            ViewData["Message"] = article;
+
+            //Create views for postname???
+            return View();
+        }
+
+        // blogpost/comment
+        // category
+        // archive/category
+        // archive/month
+        // email
+        // search
+        // timeline ????
+        // sitemap
+        // rss
+        // atom
+
+
+
+        public IActionResult Login()
+        {
+            ViewData["Message"] = "Your application login page.";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
